fcef96299729c33382bb3711024442f23e892b8f
Marcos Ruales <markosrua1994@gmail.com>
2022-03-31 23:38:04 -0500

    prod be

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    View::addExtension('html','php');
+    return View::make('index');
 });
+
